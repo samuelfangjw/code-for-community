dc15575903dec98ef9d4bb1770977e398457a34b
Samuel Fang <samuelfangjw@gmail.com>
2020-07-11 17:21:36 +0800

    added links to kickstart pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,6 +114,7 @@ module.exports = {
     },
     `gatsby-plugin-smoothscroll`,
     `react-parallax`,
+    `gatsby-plugin-catch-links`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/lessons/LessonTemplate.js
+++ src/components/lessons/LessonTemplate.js
@@ -1,11 +1,30 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Sidebar from "./sidebar.js"
 import styled from "styled-components"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import { MDXProvider } from "@mdx-js/react"
 
 const Content = styled.div`
   margin: 20px;
+`
+
+const LinkRight = styled(Link)`
+  border: 1px solid black;
+  text-decoration: none;
+  color: black;
+  padding: 10px;
+  border-radius: 5px;
+  float: right;
+`
+
+const LinkLeft = styled(Link)`
+  border: 1px solid black;
+  text-decoration: none;
+  color: black;
+  padding: 10px;
+  border-radius: 5px;
+  float: left;
 `
 
 export default function Template({
@@ -28,16 +47,18 @@ export default function Template({
     return () => window.removeEventListener("resize", handleWindowResize)
   }, [])
 
+  const shortcodes = { LinkRight, LinkLeft }
+
   const content = (
     <Content>
       <h1>{frontmatter.title}</h1>
-      <MDXRenderer style={{}}>{data.mdx.body}</MDXRenderer>
+      <MDXProvider components={shortcodes}>
+        <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      </MDXProvider>
     </Content>
   )
 
-  return (
-    <Sidebar programme={frontmatter.programme} content={content}/>
-  )
+  return <Sidebar programme={frontmatter.programme} content={content} />
 }
 export const pageQuery = graphql`
   query($slug: String!) {
